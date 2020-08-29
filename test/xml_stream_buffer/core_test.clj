de25a570e6d57d1481df50615c1f9225b5b18af4
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-29 22:05:09 +0200

    initial commit

---
+++ test/xml_stream_buffer/core_test.clj
@@ -0,0 +1,7 @@
+(ns xml-stream-buffer.core-test
+  (:require [clojure.test :refer :all]
+            [xml-stream-buffer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
