409f36da54655b294fb97cdf7b23c3806f10574c
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-29 22:54:37 +0200

    initial commit

--- src/xml_stream_buffer/core.clj
+++ src/xml_stream_buffer/core.clj
@@ -1,6 +1,12 @@
-(ns xml-stream-buffer.core)
+(ns xml-stream-buffer.core
+  (:require [clojure.data.xml :as xml :refer [parse]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parsable []
+  (with-open [input (java.io.FileInputStream. "parsable.xml")]
+    (parse input :include-node? #{:element})))
+
+(defn too-big []
+  (with-open [input (java.io.FileInputStream. "too-big.xml")]
+    (parse input)))
+
+
